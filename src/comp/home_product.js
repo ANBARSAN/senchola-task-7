86d4d6f27bb2e119d7954f95afe4d5928d8e5878
ANBARASAN <anbarasan15300@gmail.com>
2023-09-22 13:28:30 +0530

    07-taskName

---
+++ src/comp/home_product.js
@@ -0,0 +1,114 @@
+import Log1 from "../image/p1.webp";
+import Log2 from "../image/p2.webp";
+import Log3 from "../image/p3.webp";
+import Log4 from "../image/p4.webp";
+import Log5 from "../image/p5.webp";
+import Log6 from "../image/p6.webp";
+import Log7 from "../image/p7.webp";
+import Log8 from "../image/p8.webp";
+import Log9 from "../image/p9.jpg";
+import Log10 from "../image/p10.jpg";
+import Log11 from "../image/p11.jpg";
+import Log12 from "../image/p12.jpg";
+
+const Homeproduct =
+[
+    {
+        id: 1,
+        Name: 'product1',
+        price: '26',
+        image: Log1, 
+        cat: 'tv',
+        type: 'new',
+        
+    },
+    {
+        id: 2,
+        Name: 'product2',
+        price: '96',
+        image: Log2,
+        cat: 'laptop',
+        type: 'new',
+    },
+    {
+        id: 3,
+        Name: 'product3',
+        price: '236',
+        image: Log3,
+        cat: 'watch',
+        type: 'new',
+    },
+    {
+        id: 4,
+        Name: 'product4',
+        price: '636',
+        image: Log9,
+        cat: 'laptop',
+        type: 'new',
+    },
+    {
+        id: 5,
+        Name: 'product5',
+        price: '98',
+        image: Log4,
+        cat: 'speaker',
+        type: 'featured',
+    },
+    {
+        id: 6,
+        Name: 'product6',
+        price: '59',
+        image: Log5,
+        cat: 'electronics',
+        type: 'featured',
+    },
+    {
+        id: 7,
+        Name: 'product7',
+        price: '956',
+        image: Log10,
+        cat: 'headphone',
+        type: 'featured',
+    },
+    {
+        id: 8,
+        Name: 'product8',
+        price: '389',
+        image: Log11,
+        cat: 'electronics',
+        type: 'featured',
+    },
+    {
+        id: 9,
+        Name: 'product9',
+        price: '67',
+        image: Log6,
+        cat: 'phone',
+        type: 'top',
+    },
+    {
+        id: 10,
+        Name: 'product10',
+        price: '76',
+        image: Log7,
+        cat: 'speaker',
+        type: 'top',
+    },
+    {
+        id: 11,
+        Name: 'product11',
+        price: '87',
+        image: Log8,
+        cat: 'speaker',
+        type: 'top',
+    },
+    {
+        id: 12,
+        Name: 'product12',
+        price: '807',
+        image: Log12,
+        cat: 'speaker',
+        type: 'top',
+    },
+]
+export default Homeproduct
